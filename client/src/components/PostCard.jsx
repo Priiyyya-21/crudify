13d888eb69d6de2987dc6c3e71e61a1558d7400f
Priiyyya-21 <priyamajhi79@gmail.com>
2023-05-29 16:51:01 +0530

    Added PostCard section

--- client/src/components/PostCard.jsx
+++ client/src/components/PostCard.jsx
@@ -1,13 +1,48 @@
-import * as React from "react";
-import Card from "@mui/material/Card";
-import CardHeader from "@mui/material/CardHeader";
-import CardMedia from "@mui/material/CardMedia";
-import CardContent from "@mui/material/CardContent";
-import Avatar from "@mui/material/Avatar";
-import Typography from "@mui/material/Typography";
-import { red } from "@mui/material/colors";
-import moment from "moment";
+import * as React from 'react';
+import Card from '@mui/material/Card';
+import CardHeader from '@mui/material/CardHeader';
+import CardMedia from '@mui/material/CardMedia';
+import CardContent from '@mui/material/CardContent';
+import Avatar from '@mui/material/Avatar';
+import IconButton, { IconButtonProps } from '@mui/material/IconButton';
+import Typography from '@mui/material/Typography';
+import { red } from '@mui/material/colors';
+
+import MoreVertIcon from '@mui/icons-material/MoreVert';
+
+
 
 export default function PostCard(props) {
-  return "Home";
+ const{user,id,title,content,image,timestamp} =props.post;
+
+  return (
+    <Card sx={{ width: "100" }} id={id}>
+      <CardHeader
+        avatar={
+          <Avatar sx={{ bgcolor: red[500] }} aria-label="recipe">
+            {user.slice(0,1)}
+          </Avatar>
+        }
+        action={
+          <IconButton aria-label="settings">
+            <MoreVertIcon />
+          </IconButton>
+        }
+        title={title}
+        subheader={timestamp}
+      />
+      <CardMedia
+        component="img"
+        height="100%"
+        image={image}
+        alt={user}
+      />
+      <CardContent>
+        <Typography variant="body2" color="text.secondary">
+          {content}
+        </Typography>
+      </CardContent>
+      
+    </Card>
+  );
 }
