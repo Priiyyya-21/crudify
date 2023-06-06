802551d10e0870478ca5c05fc22cdbdc356a7dbf
Priiyyya-21 <priyamajhi79@gmail.com>
2023-06-06 13:52:46 +0530

    updated delete and update part

--- client/src/components/Actions.jsx
+++ client/src/components/Actions.jsx
@@ -31,7 +31,7 @@ export default function Actions({ id }) {
         })
         const data = await res.json();
         if(res.ok){
-            alert("Deleted successfully ! ")
+            alert(" Blog Deleted successfully ! ")
         }else{
             console.log(data);
         }
